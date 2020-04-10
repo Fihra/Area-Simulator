a5d79ab28c93282c833c11bb7a1a7d584a120855
Fabian <fabrofabian@gmail.com>
2020-04-10 01:24:56 -0700

    Making Enemy AI

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    private Transform myTransform;
+
+    public int moveSpeed;
+
+    public GameObject EnemyProjectile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float spawnLocation = Random.Range(-1.80f, 1.80f);
+        myTransform = transform;
+        myTransform.position = new Vector3(spawnLocation, 5, -5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myTransform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
+        
+        if(myTransform.position.y < -5.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
