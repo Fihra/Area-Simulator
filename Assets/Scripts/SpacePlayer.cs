f7c0ca527309ab1f534433508202f42dbfd3f473
Fabian <fabrofabian@gmail.com>
2020-04-09 18:25:32 -0700

    added SpacePlayer movement passing through walls

--- Assets/Scripts/SpacePlayer.cs
+++ Assets/Scripts/SpacePlayer.cs
@@ -14,7 +14,7 @@ public class SpacePlayer : MonoBehaviour
         myTransform = transform;
         //Spawn point
         //Position to be at -3, -3, -1 (x, y, z)
-        myTransform.position = new Vector3(-3, -3, -1);
+        myTransform.position = new Vector3(-1, -3, -1);
 
     }
 
@@ -23,5 +23,20 @@ public class SpacePlayer : MonoBehaviour
     {
         //Move the player left and right
         myTransform.Translate(Vector3.right * playerSpeed * Input.GetAxisRaw("Horizontal") * Time.deltaTime);
+
+        //Make the player wrap
+        // if the player is at x = -2, then it should appear at x=2. Vice Versa
+        // if the player object is positioned at x=4.5, then the game object will be 
+        // be positioned at x = -4.5
+
+        if(myTransform.position.x > 4.5f)
+        {
+            myTransform.position = new Vector3(-4.5f, myTransform.position.y, myTransform.position.z);
+        }
+
+        else if(myTransform.position.x  < -4.5f)
+        {
+            myTransform.position = new Vector3(4.5f, myTransform.position.y, myTransform.position.z);
+        }
     }
 }
