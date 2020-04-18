75ffd67a161bf37765bce8cd84b2e590234e2794
Fabian <fabrofabian@gmail.com>
2020-04-18 16:32:22 -0700

    customized the score UI

--- Assets/Scripts/MainGUI.cs
+++ Assets/Scripts/MainGUI.cs
@@ -1,13 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainGUI : MonoBehaviour
 {
-    public float x = 50,y = 50, width = 400, height = 100;
+    //public float x = 50,y = 50, width = 400, height = 100;
 
-    void OnGUI()
+    //void ongui()
+    //{
+    //    gui.box(new rect(x, y, width, height), "score: " + spaceplayer.score + " lives: " + spaceplayer.playerlives);    
+    //}
+
+    public static int scoreValue = 0;
+    Text score;
+
+    void Start()
     {
-        GUI.Box(new Rect(x, y, width, height), "Score: " + SpacePlayer.score + " Lives: " + SpacePlayer.playerLives);    
+        score = GetComponent<Text>();    
+    }
+
+    void Update()
+    {
+        //score.text = "Score: " + scoreValue;
+        score.text = $"Score: {SpacePlayer.score}";
     }
 }
