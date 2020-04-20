e3c3468f397702dd35a0d12ebd7318254c4a70da
Fabian <fabrofabian@gmail.com>
2020-04-20 00:02:35 -0700

    Created a main menu and it functions

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject instructionsPage;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+    void Update()
+    {
+        Debug.Log(instructionsPage.activeInHierarchy == true);
+        if (instructionsPage.activeInHierarchy == true && Input.GetKeyDown("space"))
+        {
+            PlayGame();
+        }
+    }
+}
