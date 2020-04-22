c622775a18ecc39db62ca732cdeb37b58c65c54c
Fabian <fabrofabian@gmail.com>
2020-04-21 21:29:41 -0700

    implemented player and asteroid sfx and title and gameplay music in wwise

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,48 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID ASTEROID_EXPLODE = 4179480828U;
+        static const AkUniqueID EARTH_DAMAGE = 436641829U;
+        static const AkUniqueID FINAL_PHASE = 4291438327U;
+        static const AkUniqueID LASER = 3982605422U;
+        static const AkUniqueID PHASE_I_THEME = 844214104U;
+        static const AkUniqueID PHASE_II_THEME = 3259071049U;
+        static const AkUniqueID PHASE_III_THEME = 674754006U;
+        static const AkUniqueID PHASE_IV_THEME = 2394473770U;
+        static const AkUniqueID PLAYER_DAMAGE = 2074073782U;
+        static const AkUniqueID PLAYER_DEATH = 3083087645U;
+        static const AkUniqueID TITLE_THEME = 2521525023U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
