2b1b2c4850a302ac5e1fd9a1f57a4ae33b7db676
Fabian <fabrofabian@gmail.com>
2020-04-22 19:45:37 -0700

    implemented title music, gameplay music, and player sfx

---
+++ Assets/Scripts/GameplayMusic.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayMusic : MonoBehaviour
+{
+    public List<AK.Wwise.Event> levelMusic = new List<AK.Wwise.Event>();
+    AK.Wwise.Event currentMusic;
+
+    void Awake()
+    {
+         
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentMusic = levelMusic[0];
+        currentMusic.Post(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerSFX.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSFX : MonoBehaviour
+{
+    public AK.Wwise.Event playerShoot;
+    public AK.Wwise.Event playerDamage;
+    public AK.Wwise.Event playerDeath;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("space"))
+        {
+            playerShoot.Post(gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Enemy"))
+        {
+            if(SpacePlayer.playerHealth < 1)
+            {
+                playerDeath.Post(gameObject);
+            }
+            else
+            {
+                playerDamage.Post(gameObject);
+            }
+            
+        }
+    }
+}
