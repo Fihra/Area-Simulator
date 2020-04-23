9f584449bfc1b8dca9f34b7beaa060f5b113e8eb
Fabian <fabrofabian@gmail.com>
2020-04-22 21:50:25 -0700

    implemented AsteroidExplode SFX

---
+++ Assets/Scripts/AsteroidSFX.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidSFX : MonoBehaviour
+{
+    public AK.Wwise.Event asteroidExplode;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Projectile") || other.gameObject.CompareTag("Player"))
+        {
+            asteroidExplode.Post(gameObject);
+        }
+    }
+}
